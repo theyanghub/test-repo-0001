d821e62a2d2e3fca2c79c13d70b8b66593f4639e
Sarah <33396785+saralavanchy@users.noreply.github.com>
2023-05-26 15:33:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [personalInformation, setPersonalInformation] = useState({
+    name: "",
+    surname:"",
+    id: "",
+    address: ""
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Your personal information</header>
+        <form>
+          <input type="text" label="name" placeholder="name" onChange={e => setPersonalInformation({...personalInformation, name: e.target.value})}/>
+          <input type="text" label="surname" placeholder="surname" onChange={e => setPersonalInformation({...personalInformation, surname: e.target.value})}/>
+          <input type="text" label="id" placeholder="your personal id" onChange={e => setPersonalInformation({...personalInformation, id: e.target.value})}/>
+          <input type="text" label="address" placeholder="your home address" onChange={e => setPersonalInformation({...personalInformation, address: e.target.value})}/>
+         <button type="submit">save your information</button> 
+        </form>    
     </div>
   );
 }
